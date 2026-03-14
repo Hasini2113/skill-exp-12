390962ac580e9197ad54be655dc3724bb9e32215
Hasini2113 <hasinipolisetty21@gmail.com>
2026-03-14 19:47:34 +0530

    Second Commit

---
+++ src/components/StudentList.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+function StudentList() {
+
+  const [students, setStudents] = useState([]);
+
+  const fetchStudents = () => {
+    axios.get("http://localhost:8080/students")
+      .then(res => setStudents(res.data));
+  };
+
+  useEffect(() => {
+    fetchStudents();
+  }, []);
+
+  const deleteStudent = (id) => {
+    axios.delete(`http://localhost:8080/students/${id}`)
+      .then(() => fetchStudents());
+  };
+
+  return (
+    <div>
+      <h2>Student List</h2>
+
+      <table border="1">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Course</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {students.map((s) => (
+            <tr key={s.id}>
+              <td>{s.name}</td>
+              <td>{s.email}</td>
+              <td>{s.course}</td>
+              <td>
+                <button onClick={() => deleteStudent(s.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    </div>
+  );
+}
+
+export default StudentList;
